be6d3285992e9db76a4e7d5a3131d7e89a4bea45
Threads8 <kakarotsaiyan863@gmail.com>
2025-08-10 18:32:26 +0530

    changes

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import TerminalComponent from './comp/Terminal';
-import RAN from './comp/ran';
+import logo from "./logo.svg";
+import "./App.css";
+import TerminalComponent from "./comp/Terminal";
+import RAN from "./comp/ran";
 
 function App() {
   return (
+    // terminal comp
     <TerminalComponent />
   );
 }
